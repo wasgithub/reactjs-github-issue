dd946a66f01af15c93dab45a06bc94dbffa4f40f
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-04-20 22:30:49 -0300

    LISTANDO REPOSITORIOS

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -71,3 +71,25 @@ export const SubmitButton = styled.button.attrs((props) => ({
       }
     `}
 `;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      text-decoration: none;
+      color: #7179c1;
+    }
+  }
+`;
